4dd4e4388308dad8575bc3d579e4c99315ade556
Ciprian Serban <ciprian.serban@ascentcore.com>
2026-03-29 23:45:00 +0300

    [ciprian-serban] fizz_buzz solution

--- 4_control_flows/basics/fizz_buzz.py
+++ 4_control_flows/basics/fizz_buzz.py
@@ -7,3 +7,21 @@
 # Example
 # n = 15
 # Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+# Ciprian Serban
+def fizz_buzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return result
+
+n = 15
+output = fizz_buzz(n)
+print(output)
