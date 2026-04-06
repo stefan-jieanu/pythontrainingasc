4a4265c5129ccefd194a558cf29388e522b27d5e
Horia Balc <horia.balc@ascentcore.com>
2026-04-06 12:51:43 +0300

    data_structures

--- 5_data_structures/list_comprehension.py
+++ 5_data_structures/list_comprehension.py
@@ -17,3 +17,26 @@
 # (1) Return the average age of all people, or (optionally) all people under a given age.
 # (2) Return a set of the different hobbies enjoyed by people in our database.
 # (3) Count how many hobbies each person has
+
+def average_age(people, max_age=None):
+    if max_age is not None:
+        people = [person for person in people if person['age'] < max_age]
+    if not people:
+        return 0
+    return sum(person['age'] for person in people) / len(people)
+
+def unique_hobbies(people):
+    return set(hobby for person in people for hobby in person['hobbies'])
+
+def count_hobbies(people):
+    return {person['name']: len(person['hobbies']) for person in people}
+
+all_people = [{'name':'Reuven', 'age':48, 'hobbies':['Python', 'cooking', 'reading']},
+                {'name':'Atara', 'age':27, 'hobbies':['horses', 'cooking', 'art', 'reading']},
+                {'name':'Shikma', 'age':13, 'hobbies':['Python', 'piano', 'cooking', 'reading']},
+                {'name':'Amotz', 'age':60, 'hobbies':['biking', 'cooking']}]        
+
+print(average_age(all_people))
+print(average_age(all_people, max_age=30))
+print(unique_hobbies(all_people))
+print(count_hobbies(all_people))
--- 5_data_structures/word_frequency_counter.py
+++ 5_data_structures/word_frequency_counter.py
@@ -4,3 +4,42 @@
 # Example
 # text = "apple banana apple cherry banana apple"
 # Output: {'apple': 3, 'banana': 2, 'cherry': 1}
+from collections import Counter
+
+def word_frequency_counter(text):
+    word_counts = {}
+    words = text.split()
+    
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+            
+    return word_counts
+
+def word_frequency_counter1(text):
+    word_counts = {}
+    words = text.split()
+    
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+            
+    return word_counts
+
+def word_frequency_counter2(text):
+    word_counts = {}
+    words = text.split()
+    single_word = set(words)
+    for word in single_word:
+        word_counts[word] = words.count(word)
+            
+    return word_counts
+
+def word_frequency_counter3(text):
+    return dict(Counter(text.split()))
+
+print(word_frequency_counter("apple banana apple cherry banana apple"))
+print(word_frequency_counter1("apple banana apple cherry banana apple"))
+print(word_frequency_counter2("apple banana apple cherry banana apple"))
+print(word_frequency_counter3("apple banana apple cherry banana apple"))
